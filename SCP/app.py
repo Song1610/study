4b3c4b2ac84e7b5ecc9028db7305061776bb09a5
Song1610 <twd5741@gmail.com>
2025-02-19 15:39:42 +0900

    update

--- SCP/app.py
+++ SCP/app.py
@@ -1,9 +1,13 @@
 from flask import Flask
 app = Flask(__name__)
 
+# @app.route('/')
+# def index():
+#    return render_template('index.html')
+
 @app.route('/')
 def index():
-   return render_template('index.html')
+   return 'hello world!'
 
 if __name__ == '__main__':  
    app.run('0.0.0.0', port=5000, debug=True)
